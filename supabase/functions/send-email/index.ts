835f85574e4177f05ca6efcf96a478153d9f29b9
ronnyads <libertecdados@gmail.com>
2026-03-23 20:25:16 -0300

    fix: CORS origin to www.adsliberty.com

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -1,7 +1,7 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://adsliberty.com',
+  'Access-Control-Allow-Origin': 'https://www.adsliberty.com',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
